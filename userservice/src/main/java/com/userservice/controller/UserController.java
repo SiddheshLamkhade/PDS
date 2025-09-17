2d3a3cf2b15b911d9d475b30af9d132cf2e1426c
PratikPDahale <dahalepratik2004@gmail.com>
2025-09-17 20:05:55 +0530

    Modified userservice.properties

--- userservice/src/main/java/com/userservice/controller/UserController.java
+++ userservice/src/main/java/com/userservice/controller/UserController.java
@@ -1,20 +1,18 @@
 package com.userservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.userservice.dto.RegisterRequest;
-import com.userservice.dto.UserResponse;
-import com.userservice.model.User;
-import com.userservice.service.UserService;
-import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.userservice.dto.RegisterRequest;
+import com.userservice.dto.UserResponse;
+import com.userservice.service.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/users")
